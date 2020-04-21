cd25db8b5078992244bb52b5e8d66886db7df5cc
Glitch (hello-express) <none>
2020-04-21 09:17:13 +0000

    🔨🎌 Checkpoint ./server.js:15233164/539 ./db.json:15233164/17 ./views/index.pug:15233164/100 ./views/add/index.pug:15233164/228

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.get('/books', (req, res) => {
   res.render("index", {
-    bookList: db.get("")
+    bookList: db.get("books").value()
   });
+});
+
+app.get('/books/add', (req, res) => {
+  res.render("add/index")
+});
+
+app.post("/books/add", (req, res) =>{
+  db.get("books").push(req.body).write();
+  res.redirect("/books");
 });
 
 // listen for requests :)
