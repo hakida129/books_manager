32de99c79198e3f6e2285f6d36e9ea6165aad095
Glitch (bai15-authentication) <none>
2020-04-27 12:04:02 +0000

    ⏳😄 Checkpoint ./controllers/auth.controller.js:15233164/3502 ./controllers/book.controller.js:15233164/2154 ./controllers/cart.controller.js:15233164/440 ./controllers/store.controller.js:15233164/296 ./controllers/transaction.controller.js:15233164/2296 ./controllers/transactionMenu.controller.js:15233164/758 ./controllers/user.controller.js:15233164/3554 ./middlewares/admin.middleware.js:15233164/614 ./middlewares/auth.middleware.js:15233164/734 ./middlewares/session.middleware.js:15233164/321 ./router/auth.router.js:15233164/474 ./router/book.router.js:15233164/950 ./router/cart.router.js:15233164/204 ./router/store.router.js:15233164/191 ./router/transaction.router.js:15233164/678 ./router/transactionMenu.router.js:15233164/406 ./router/user.router.js:15233164/1213 ./views/books/update.pug:15233164/797 ./views/layouts/common.pug:15233164/3562 ./views/store/index.pug:15233164/437 ./views/transactions/create.pug:15233164/1174 ./views/transactions/index.pug:15233164/2347 ./views/transactions/menu.pug:15233164/696 ./views/users/add.pug:15233164/1756 ./views/users/index.pug:15233164/1363 ./views/users/profile.pug:15233164/1466 ./views/users/updateAvatar.pug:15233164/439 ./views/index.pug:15233164/352 ./db.js:15233164/372 ./db.json:15233164/78112 ./server.js:15233164/3283 ./package.json:15233164/112961

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
-var cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 const port = 3000
 
 const bookRouter = require('./router/book.router');
 const userRouter = require('./router/user.router');
-const transactionRouter = require('./router/transaction.router')
+const transactionRouter = require('./router/transaction.router');
 const authRouter = require("./router/auth.router");
-const menuRouter = require("./router/transactionMenu.router")
+const menuRouter = require("./router/transactionMenu.router");
+const storeRoute = require("./router/store.router");
+const cartRoute = require('./router/cart.router');
+
 const authMiddleware = require("./middlewares/auth.middleware");
 const adminMiddleware = require('./middlewares/admin.middleware');
+const sessionMiddleware = require('./middlewares/session.middleware');
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) 
 // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SESSION_SECRET))
+app.use(sessionMiddleware)
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -25,11 +30,14 @@ app.get('/', (req, res) =>{res.render("index")})
 
 app.use(express.static('public'))
 
+
 app.use('/auth', authRouter);
 app.use('/books', authMiddleware.authRequire, adminMiddleware.requireRole(true), bookRouter);
 app.use('/users', authMiddleware.authRequire, adminMiddleware.requireRole(true), userRouter);
-app.use('/transactions', authMiddleware.authRequire, adminMiddleware.requireRole(true), transactionRouter)
-app.use('/transaction', authMiddleware.authRequire, adminMiddleware.requireRole(false), menuRouter)
+app.use('/transactions', authMiddleware.authRequire, adminMiddleware.requireRole(true), transactionRouter);
+app.use('/transaction', authMiddleware.authRequire, adminMiddleware.requireRole(false), menuRouter);
+app.use('/store', storeRoute);
+app.use('/cart', cartRoute);
 
 // listen for requests :)
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
